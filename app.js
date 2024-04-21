1cb8657da172aefe25d06ff67b7ee7a9932ca531
RicardoAJimenezR <ricardoa.jimenezr@gmail.com>
2024-04-20 21:58:24 -0400

    dummycall function was created, to be able to use the fetch function

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+// Función para simular una llamada ficticia
+function dummyCall() {
+  // Simulamos una respuesta exitosa después de un breve retraso
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // Datos ficticios
+      const dummyData = { 
+        gpsId: 1,
+        gpsSerial: 'Dummy Data',
+        unitAssignedTo: 'This is a dummy response from the server.',
+	    simCardId: '123456',
+	    simCardUid: '457849'
+      };
+      // Resolvemos la promesa con los datos ficticios
+      resolve(dummyData);
+    }, 1000); // Simulamos un retraso de 1 segundo
+  });
+}
 
+// Llamamos a la función dummyCall y manejamos la respuesta
+dummyCall()
+  .then(data => {
+    console.log('Respuesta ficticia recibida:', data);
+    // Aquí puedes manipular los datos recibidos
+  })
+  .catch(error => {
+    console.error('Hubo un error al procesar la llamada ficticia:', error);
+  });
