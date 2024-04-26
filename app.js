309f465fd62ddb75fb3c0016c6bf38b5db8ca199
RicardoAJimenezR <ricardoa.jimenezr@gmail.com>
2024-04-26 01:14:44 -0400

    Agregando la funcion DummyCall para generar la data de la tabala

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
+let dummyData;
+
 // Función para simular una llamada ficticia
 function dummyCall() {
   // Simulamos una respuesta exitosa después de un breve retraso
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       // Datos ficticios
-      const dummyData = { 
-        gpsId: 1,
-        gpsSerial: 'Dummy Data',
-        unitAssignedTo: 'This is a dummy response from the server.',
-	    simCardId: '123456',
-	    simCardUid: '457849'
-      };
+      dummyData = [
+        { gpsId: 1, gpsSerial: '123', unitAssignedTo: 'Unidad 1', simCardId: '456', simCardUid: '789' },
+        { gpsId: 2, gpsSerial: '456', unitAssignedTo: 'Unidad 2', simCardId: '789', simCardUid: '012' },
+        { gpsId: 3, gpsSerial: '789', unitAssignedTo: 'Unidad 3', simCardId: '012', simCardUid: '345' }
+      ];
       // Resolvemos la promesa con los datos ficticios
       resolve(dummyData);
     }, 1000); // Simulamos un retraso de 1 segundo
   });
 }
 
-// Llamamos a la función dummyCall y manejamos la respuesta
-dummyCall()
-  .then(data => {
-    console.log('Respuesta ficticia recibida:', data);
-    // Aquí puedes manipular los datos recibidos
-  })
-  .catch(error => {
-    console.error('Hubo un error al procesar la llamada ficticia:', error);
+// Función para crear una celda
+function crearCelda(valor) {
+  const celda = document.createElement('td');
+  celda.textContent = valor;
+  return celda;
+}
+
+// Función para crear una fila
+function crearFila(item) {
+  const fila = document.createElement('tr');
+  Object.values(item).forEach(val => {
+    const celda = crearCelda(val);
+    fila.appendChild(celda);
   });
+  return fila;
+}
+
+// Función para llenar la tabla
+async function llenarTabla() {
+  try {
+    // Haciendo la llamada ficticia
+    const data = await dummyCall();
+
+    // Obtener el cuerpo de la tabla donde se agregarán los datos
+    const tabla = document.querySelector('#tablaDatos table tbody');
+
+    // Vaciar el cuerpo de la tabla antes de agregar nuevos datos
+    tabla.innerHTML = '';
+
+    // Generar las filas de la tabla con los datos ficticios
+    data.forEach(item => {
+      const fila = crearFila(item);
+      tabla.appendChild(fila);
+    });
+  } catch (error) {
+    console.error('Hubo un problema con la llamada ficticia:', error);
+  }
+}
+
+// Llamar a la función para llenar la tabla al cargar la página
+document.addEventListener('DOMContentLoaded', llenarTabla);
